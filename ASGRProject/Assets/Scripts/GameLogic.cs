6dbfe933b0028ae9632c4e4541e0ea0fad5addb2
Felipe Rubin <felipe.rubin@acad.pucrs.br>
2019-10-03 15:55:22 -0300

    Adding stage panel

---
+++ ASGRProject/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+    public Text stageText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        stageText.text = "Remaining Enemies: ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
--- ASGRProject/Assets/Scripts/Player.cs
+++ ASGRProject/Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     public Image spBar;
     public Image hpBar;
+    
 
     //public List<GameObject>
 
